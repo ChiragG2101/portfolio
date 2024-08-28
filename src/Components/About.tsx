3d2a153113e8d71aeeeb43f835f7a1c226403f14
Chirag Gupta <chiragg593@gmail.com>
2024-08-29 01:16:20 +0530

    resume changes

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -61,7 +61,7 @@ const About = () => {
             </Button>
             <Button
               component="a"
-              href="Resume.pdf"
+              href="chirag_chiragg593_gmail_com_resume.pdf"
               download={Info.name}
               className="focus-visible:!outline-none !text-primaryColor !w-fit xs-mx:!w-[46%]"
               size={btn}
